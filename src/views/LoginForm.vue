86679b1fd53ef1b7752f0586f5785119957c1c4c
luml <1520622848@qq.com>
2021-09-15 18:19:01 +0800

    :octocat

---
+++ src/views/LoginForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <BaseInput label="Email" type="email" v-model="email" :error="errorMessage" />
+    <BaseInput label="Password" type="password" v-model="password" :error="errorMessage"/>
+    <BaseButton type="submit" class="-fill-gradient" />
+  </form>
+</template>
+
+<script>
+import { useField, useForm } from 'vee-validate'
+export default {
+  setup () {
+    function onSubmit () {
+      alert('Submited!')
+    }
+
+    const validations = {
+      email: value = () => {}
+      
+    }
+
+    const email = useField('email', (value) => {
+      if (!value) return 'This field is required'
+      return true
+    })
+
+    const password = useField('password', (value) => {
+      if (!value) return 'This field is required'
+      return true
+    })
+
+    return {
+      onSubmit,
+      email: email.value,
+      password: password.value
+    }
+  }
+}
+</script>
